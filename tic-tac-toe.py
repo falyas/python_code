3c3f8fe91d60c0329b6fe6d4eb2af35262cfda12
farah <49843440+falyas@users.noreply.github.com>
2020-08-02 14:10:51 -0700

    Solved tic-tac-toe problem :)
    finished VictoryFor function and the program entry
    if I have more time, I would implement an exit behavior

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,7 @@
 # author: farah alyasari
 
 # packages
-import random
+import random, sys
 
 # 3 by 3 board
 board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
@@ -33,13 +33,14 @@ def EnterMove(board):
     while enteredMove == True:
         try:
             move = int(input("Pick a move: "))
-            if move > max or move < min:
-                print("Illegal move")
-            enteredMove == False
+            assert move < max and move > min
+            enteredMove = False
+        except AssertionError:
+            print("Illegal move")
         except ValueError:
             print("Illegal value")
-        except:
-            print("unknown error")
+        #except:
+            #print("error")
     found = False
     for row in board:
         if move in row:
@@ -56,7 +57,6 @@ def EnterMove(board):
 #
 def MakeListOfFreeFields(board):
     list = []
-    counter = 0
     for row in board:
         for col in row:
             if col != player_symbol and col != computer_symbol:
@@ -66,27 +66,27 @@ def MakeListOfFreeFields(board):
     return list
 
 #
-# the function browses the board and builds a list of occupied fields for a given symbol;
-# the list consists of tuples, while each tuple is a pair of row and column numbers
-#
-def MakeListOfSignFields(board, sign):
-    list = []
-    counter = 0
-    for row in board:
-        for col in row:
-            if col == symbol:
-                row_position = board.index(row)
-                col_position = row.index(col)
-                list.append((row_position,col_position))
-    return list
-
-#
 # the function analyzes the board status in order to check if
-# the player using 'O's or 'X's has won the game
+# the player using the specified symbol has won the game
 #
-def VictoryFor(board, sign):
-    sign_fields = MakeListOfSignFields(board, sign)
-    for i in range(len(board)):
+def VictoryFor(board, symbol):
+    #check if there's a win
+    cross_1 = cross_2 = True
+    for rc in range(3):
+        # checking each possible row combination
+        if board[rc][0] == symbol and board[rc][1] == symbol and board[rc][2] == symbol:
+            return True
+        # check each column
+        if board[0][rc] == symbol and board[1][rc] == symbol and board[2][rc] == symbol:
+            return True
+        # check the diagnols
+        if board[rc][rc] != symbol:
+            cross_1 = False
+        if board[2-rc][2-rc] != symbol:
+            cross_2 = False
+    if cross_1 or cross_2:
+        return True
+    return False
 
 #
 # the function draws the computer's move and updates the board
@@ -100,17 +100,39 @@ def DrawMove(board):
         col = move[1]
         board[row][col] = computer_symbol
     except IndexError:
-        print("The game should have ended")
+        print("The game should have ended. We shouldn't reach here")
     except:
-        print("unkown error")
+        print("unkown error. We shouldn't reach here")
 
 #
-# Program entry
+# Program entry and program exit
 #
 def TicTacToe(board):
-    #this is an infinite loop
-    while True:
-        DrawMove(board)
+    free_fields = MakeListOfFreeFields(board)
+    computer_turn = True
+    computer_status = False
+    player_status = False
+    while(len(free_fields) > 0):
+        if computer_turn:
+            print("computer turn: ")
+            DrawMove(board)
+            computer_status = VictoryFor(board, computer_symbol)
+        else:
+            EnterMove(board)
+            player_status = VictoryFor(board, player_symbol)
+        #check victory
+        if player_status == True:
+            DisplayBoard(board)
+            print("You won")
+            break
+        if computer_status == True:
+            DisplayBoard(board)
+            print("Computer won")
+            break
+        computer_turn = not computer_turn
+        free_fields = MakeListOfFreeFields(board)
         DisplayBoard(board)
-        EnterMove(board)
-        DisplayBoard(board)
+    if computer_status == False and player_status == False:
+        print("Tie")
+
+TicTacToe(board)
